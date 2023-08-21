943e8ba8d31e57bb850aa90e48d693ee34e9d754
Giulian Capasso <142629447+giulian-capasso@users.noreply.github.com>
2023-08-21 21:06:38 +0200

    8_R_code_LiDAR.r

--- !!!!!8_R_code_LiDAR.r
+++ !!!!!8_R_code_LiDAR.r
@@ -1,4 +1,4 @@
-# R code for visualise and analysing LiDAR data
+# R code to visualize and analysing LiDAR data
 
 library(raster)
 library(ggplot2)
@@ -23,6 +23,10 @@ ggplot() +
   geom_raster(chm_2013, mapping =aes(x=x, y=y, fill=layer)) + 
   scale_fill_viridis() +
   ggtitle("CHM 2013 San Genesio/Jenesien")
+
+# blue =grass
+# houses (10 - 20) 
+# woods = lighter (20 - 40)
   
  dsm_2004 <- raster("2004Elevation_DigitalElevationModel-2.5m.tif")
   
@@ -35,10 +39,13 @@ ggplot() +
   scale_fill_viridis() +
   ggtitle("CHM 2004 San Genesio/Jenesien")
   
+#comparison
+
 difference_chm <- chm_2013-chm_2004
-  
-# bisogna cambiare la risoluzione del dato -> da risoluzione più accurata a meno accurate
-# in funzione resample: su x ciò che si vuole ricampionare, su y dare la risoluzione dell'altra immagine (all'immagine segnata su x)
+
+# got to change the data resolution -> from more accurate to less accurate resolution
+# "resample" function: "x" what you want to resample, "y" the resolution of the other image 
+
 chm_2013_r <- resample(chm_2013, chm_2004)
 chm_2013_r
   
@@ -48,6 +55,9 @@ ggplot() +
   geom_raster(difference_chm, mapping =aes(x=x, y=y, fill=layer)) +
   scale_fill_viridis() +
   ggtitle("CHM difference San Genesio/Jenesien")
+# the darker areas are deforested areas
+# in the lighter, yellow areas, there is an increase in plants
+
 
 point_cloud <- readLAS("point_cloud.laz")
 plot(point_cloud)
