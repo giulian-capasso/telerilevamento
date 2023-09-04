ab495666ec8c1e13de25e215bdcda2abbe8ac0ee
Giulian Capasso <142629447+giulian-capasso@users.noreply.github.com>
2023-09-04 15:24:58 +0200

    Update 3_R_code_classification.r

--- 3_R_code_classification.r
+++ 3_R_code_classification.r
@@ -1,41 +1,44 @@
 # R_code_classification.r
 # Classification of an image
-# how to switch from continuous data to classes eg mineralogical composition
-# With Solar Orbiter Data
+# how to switch from continuous data to classes eg. mineralogical composition
+# Excercise with Solar Orbiter Data
 
 # photos taken from the space station
 # we don't have satellite images like previously (imp. never say satellite photos)
 
-# Loading required package
+# Installing required package
 install.packages("RStoolbox")
 
+# recall needed packages
 library(raster)
 # library(RStoolbox)
 
+# set working directory for MacOS
 setwd("~/Desktop/lab_/") 
 
 # data import
 so <- brick("Solar_Orbiter_s_first_views_of_the_Sun_pillars.jpg")
 # solar orbiter is the satellite that takes data on the movements of the sun
 
-# visualize RGB leves
+# visualize RGB leves with two different stretches. Lin is a linear funcion, hist exaggerate the tails
 plotRGB(so, 1, 2, 3, stretch="lin")
 plotRGB(so, 1, 2, 3, stretch="hist")
 
 # Classifying the solar data (unsupervised classification > classes decided by the software)
-#set.seed(42) to pick always the same specific pixels; used for repeating the experiment in the same manner for N times
+# set.seed(42) to pick always the same specific pixels; used for repeating the experiment in the same manner for N times
 
-soc <- unsuperClass(so, nClasses=3)
-cl <- colorRampPalette(c('yellow','black','red'))(100)
-plot(soc$map, col=cl)
+soc <- unsuperClass(so, nClasses=3) # in particular 3 classes, but it is customizable according to your data
+cl <- colorRampPalette(c('yellow','black','red'))(100) # personalized color palette
+plot(soc$map, col=cl) # plot using $ the 'rope' 
 
-# Unsupervised Classification with 20 classes
+# Unsupervised Classification with 20 classes now
 soc20 <- unsuperClass(so, nClasses=20)
 cl <- colorRampPalette(c('yellow','black','red'))(100)
 plot(soc20$map, col=cl)
 
 
-## DAY 2 Grand Canyon ##
+############################## DAY 2 Grand Canyon ##################################
+
 
 gc <- brick("dolansprings_oli_2013088_canyon_lrg.jpg")
 gc
@@ -50,7 +53,7 @@ plotRGB(gc, r=1, g=2, b=3, stretch="lin")
 # use "hist" to increase the visualizatio of the tails of the curve that is a straight line in case of "lin"
 plotRGB(gc, r=1, g=2, b=3, stretch="hist")
 
-# classification
+# classification, 2 classes
 gcclass2 <- unsuperClass(gc, nClasses=2)
 gcclass2
 
@@ -63,6 +66,7 @@ plot(gcclass2$map)
 gcclass4 <- unsuperClass(gc, nClasses=4)
 gcclass4
 
+# now plot and see
 clc <- colorRampPalette(c('yellow','red','blue','black'))(100)
 plot(gcclass4$map, col=clc)
 
