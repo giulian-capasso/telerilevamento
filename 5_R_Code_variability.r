6e7e02636b69c8a472a12b50ba5e67f6b0d046f1
Giulian Capasso <142629447+giulian-capasso@users.noreply.github.com>
2023-09-04 18:24:33 +0200

    Update 5_R_Code_variability.r

--- 5_R_Code_variability.r
+++ 5_R_Code_variability.r
@@ -6,10 +6,13 @@ library(ggplot2) # for ggplot plotting
 library(patchwork) # multiframe with ggplot2 graphs
 
 # install.packages("viridis")
-# Viridis has different and incluseive colour palettes available
-# crucial for people with color perceptions divergences
+# Viridis has different and inclusive colour palettes available
+# Color blind friendly
+# Crucial for people with color perceptions divergences
+# install or recall packages
 library(viridis) 
 
+# set working directory for MacOS
 setwd("~/Desktop/lab_/")
 
 # Exercise: import the Similaun image
@@ -22,10 +25,10 @@ similaun <- brick("sentinel.png")
 # Exercise: plot the image by the ggRGB function
 ggRGB(similaun, 1, 2, 3)
 
-# NIR on g component
+# NIR on green component
 ggRGB(similaun, 2, 1, 3)
 
-# Exercise: plot the two graphs one beside the other
+# Exercise: plot the two graphs one beside the other with ggplot 
 sim1 <- ggRGB(similaun, 1, 2, 3)
 sim2 <- ggRGB(similaun, 2, 1, 3)
 
@@ -43,22 +46,23 @@ nir <- similaun[[1]]
 # use "focal" function to calculate the statistical variability in the images 
 # we make a first calculation of the variability using the infrared
 # select the layer to start with
-sd3 <- focal(nir, matrix(1/9, 3, 3), fun=sd) #sd= standard deviation or where 68% of the values fall
+sd3 <- focal(nir, matrix(1/9, 3, 3), fun=sd) #sd = standard deviation or where 68% of the values fall
 
+# create a personalized color palette and plot
 clsd <- colorRampPalette(c('blue','green','pink','magenta','orange','brown','red','yellow'))(100) # 
 plot(sd3, col=clsd)
 
-# plotting with ggplot
+# plotting with ggplot (non RGB gg plot!) 
 ggplot() + 
 geom_raster(sd3, mapping=aes(x=x, y=y, fill=layer))
 
-# with viridis
+# with viridis and more inclusive colors
 ggplot() + 
 geom_raster(sd3, mapping =aes(x=x, y=y, fill=layer)) + 
 scale_fill_viridis() +
 ggtitle("Standard deviation by viridis package")
 
-# cividis
+# cividis - exploring the viridis package 
 ggplot() + 
 geom_raster(sd3, mapping =aes(x=x, y=y, fill=layer)) + 
 scale_fill_viridis(option = "cividis") +
@@ -97,12 +101,8 @@ clsd <- colorRampPalette(c('blue','green','pink','magenta','orange','brown','red
 # Plot with ggplot but it is not RBG so we need a different code
 
 ggplot() +
-geom_raster(varSim, mapping = aes(x=x, y=y, fill=layer) #layer scanner satellite
+geom_raster(varSim, mapping = aes(x=x, y=y, fill=layer)) #layer scanner satellite
 
-            # so in order to plot non RGB images with ggplot > 1) empty ggplot() + geom_raster wuth image name, + mapping for the aestetics
-#for specific color palette > Viridis > + scale_fill_viridis(option = "inferno") 
-
-geom_raster(varSim, mapping = aes(x=x, y=y, fill=layer) #layer scanner satellite
-
-            # so in order to plot non RGB images with ggplot > 1) empty ggplot() + geom_raster wuth image name, + mapping for the aestetics
-#for specific color palette > Viridis > + scale_fill_viridis(option = "inferno") 
+# so in order to plot non RGB images with ggplot 
+# 1) empty ggplot() + geom_raster with image name, + mapping for the aestetics
+# for specific color palette > Viridis > + scale_fill_viridis(option = "inferno") 
